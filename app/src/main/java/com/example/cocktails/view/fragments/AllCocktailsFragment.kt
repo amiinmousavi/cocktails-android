09465e7b5f9929b9294af8598259fb49c75613ba
amiinmousavi <seyed.mousavi03@gmail.com>
2023-04-22 03:55:10 +0200

    recyclerview cocktails

--- app/src/main/java/com/example/cocktails/view/fragments/AllCocktailsFragment.kt
+++ app/src/main/java/com/example/cocktails/view/fragments/AllCocktailsFragment.kt
@@ -4,22 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.cocktails.R
 import com.example.cocktails.application.CocktailApplication
 import com.example.cocktails.databinding.FragmentAllCocktailsBinding
 import com.example.cocktails.view.activities.AddUpdateCocktailActivity
+import com.example.cocktails.view.adapters.CocktailListAdapter
 import com.example.cocktails.viewmodel.CocktailViewModel
 import com.example.cocktails.viewmodel.CocktailViewModelFactory
 import com.example.cocktails.viewmodel.HomeViewModel
 
 class AllCocktailsFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
-
+//    private lateinit var homeViewModel: HomeViewModel
+//    private var _binding: FragmentAllCocktailsBinding? = null
+//    private val binding get() = _binding!!
+    private lateinit var binding: FragmentAllCocktailsBinding
     private val cocktailViewModel: CocktailViewModel by viewModels {
         CocktailViewModelFactory(
             (requireActivity().application as
@@ -32,45 +36,50 @@ class AllCocktailsFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    private var _binding: FragmentAllCocktailsBinding? = null
-
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentAllCocktailsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.tvNoCocktailsAddedYet
-
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+//        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+//        _binding = FragmentAllCocktailsBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//        val textView: TextView = binding.tvNoCocktailsAddedYet
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+//        return root
+        binding = FragmentAllCocktailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        Log.i("Cocktail", "onViewCreated works")
         super.onViewCreated(view, savedInstanceState)
+
+        binding.rvCocktailsList.layoutManager = GridLayoutManager(requireActivity(), 2)
+        val cocktailListAdapter = CocktailListAdapter(this@AllCocktailsFragment)
+
+        binding.rvCocktailsList.adapter = cocktailListAdapter
         cocktailViewModel.allCocktails.observe(viewLifecycleOwner) { cocktails ->
             cocktails.let {
                 for (item in it) {
-                    Log.i("Cocktail", "${item.idDrink} :: ${item.strDrink}")
+                    if(it.isNotEmpty()){
+                        binding.rvCocktailsList.visibility = View.VISIBLE
+                        binding.tvNoCocktailsAddedYet.visibility = View.GONE
+                        cocktailListAdapter.cocktailsList(it)
+                    }else{
+                        binding.rvCocktailsList.visibility = View.GONE
+                        binding.tvNoCocktailsAddedYet.visibility = View.VISIBLE
+                    }
                 }
             }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_all_cocktails, menu)
