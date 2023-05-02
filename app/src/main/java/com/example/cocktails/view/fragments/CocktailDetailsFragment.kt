a78b1dbfbbf77aefd5f3bb63978419095fc40025
amiinmousavi <seyed.mousavi03@gmail.com>
2023-05-02 22:36:06 +0200

    cocktaildetailfragment completed

--- app/src/main/java/com/example/cocktails/view/fragments/CocktailDetailsFragment.kt
+++ app/src/main/java/com/example/cocktails/view/fragments/CocktailDetailsFragment.kt
@@ -7,9 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
-import com.example.cocktails.R
+import com.bumptech.glide.Glide
+import com.example.cocktails.databinding.FragmentCocktailDetailsBinding
+import java.io.IOException
 
 class CocktailDetailsFragment : Fragment() {
+
+    private var binding : FragmentCocktailDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +23,37 @@ class CocktailDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cocktail_details, container, false)
+        binding = FragmentCocktailDetailsBinding.inflate(inflater, container, false)
+        return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: CocktailDetailsFragmentArgs by navArgs()
-        Log.i("Cocktail Title", args.cocktailData.strDrink)
-        Log.i("Cocktail Category", args.cocktailData.strCategory)
+
+        args.let{
+            try {
+                Glide.with(requireActivity())
+                    .load(it.cocktailData.strDrinkThumb)
+                    .centerCrop()
+                    .into(binding!!.ivCocktailImage)
+            } catch(e: IOException) {
+                e.printStackTrace()
+            }
+
+            binding!!.tvTitle.text = it.cocktailData.strDrink
+            binding!!.tvAlcoholic.text = it.cocktailData.strAlcoholic
+            binding!!.tvCategory.text = it.cocktailData.strCategory
+            binding!!.tvGlass.text = it.cocktailData.strGlass
+            binding!!.tvInstruction.text = it.cocktailData.strInstructions
+            binding!!.tvIngredient.text = it.cocktailData.strIngredient
+            binding!!.tvMeasure.text = it.cocktailData.strMeasure
+            binding!!.tvDateModified.text = it.cocktailData.dateModified
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
